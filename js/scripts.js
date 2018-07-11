26924537b0c51ab62a0ac21b0508708e8fa9c711
sk7konstantin <sk7konstantin@yandex,ru>
2018-07-12 02:38:25 +0300

    Готовый проект

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,36 @@ var popup = document.querySelector(".modal-feedback");
 var close = document.querySelector(".modal-close");
 var login = popup.querySelector("[name=login]");
 var email = popup.querySelector("[name=email]");
+var comments = popup.querySelector("[name=comments]")
 var form = popup.querySelector("form");
 var overlay = document.querySelector(".modal-overlay");
+
+var isStorageSupport = true;
+var storage = "";
+  
+try {
+	storage = localStorage.getItem("login");
+} catch (err) {
+    isStorageSupport = false;
+}
 
 link.addEventListener ("click", function (evt) {
 	evt.preventDefault ();
 	popup.classList.add("modal-show");
 	overlay.classList.add("modal-overlay-show")
-	login.focus ();
+
+	if (storage) {
+		login.value = storage;
+		email.focus();
+	} else {
+		login.focus();
+	}
 });
 
 close.addEventListener ("click", function(evt) {
 	evt.preventDefault ();
 	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
 	overlay.classList.remove("modal-overlay-show")
 });
 
@@ -23,6 +40,7 @@ window.addEventListener("keydown", function (evt) {
 	if (evt.keyCode === 27) {
 		if (popup.classList.contains("modal-show")) {
 			popup.classList.remove("modal-show");
+			popup.classList.remove("modal-error")
 			overlay.classList.remove("modal-overlay-show");
 		}
 	}
@@ -32,6 +50,13 @@ window.addEventListener("keydown", function (evt) {
 form.addEventListener ("submit", function (evt) {
 	if (!login.value || !email.value) {
 	evt.preventDefault ();
-	console.log("Нужно ввести логин и пароль");
-	} 
+	popup.classList.remove("modal-error");
+	popup.offsetWidth = popup.offsetWidth;
+	popup.classList.add("modal-error");
+	// console.log("Нужно ввести логин и пароль");
+	} else {
+		if (isStorageSupport) {
+		localStorage.setItem("login", login.value);
+		}
+	}
 });
